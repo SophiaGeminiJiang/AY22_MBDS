85681ca560759e72be87789c7f612d594ff9f6c0
SophiaGeminiJiang <55688962+SophiaGeminiJiang@users.noreply.github.com>
2022-01-12 06:09:47 +0800

    Add files via upload

---
+++ Answer to Q2/Code/odefun.m
@@ -0,0 +1,11 @@
+function f = odefun(t,y)
+k1 = 100;
+k2 = 600;
+k3 = 150;
+dydt(1) = k2*y(3)+k3*y(3)-k1*y(1)*y(2);
+dydt(2) = k2*y(3)-k1*y(1)*y(2);
+dydt(3) = k1*y(1)*y(2)- k2*y(3)-k3*y(3);
+dydt(4) = k3*y(3);
+f = dydt(:);
+end
+
